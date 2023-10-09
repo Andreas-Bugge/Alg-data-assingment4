c0ffc3b9207d2e47f23f465639734c90983b55c3
Andreas-Bugge <202204219@post.au.dk>
2023-10-10 00:55:38 +0200

    Er usikker på hvilken fil der skal  modificeres

--- Week_07/binary_heap.h
+++ Week_07/binary_heap.h
@@ -38,3 +38,25 @@ class BinaryHeap {
 #include "binary_heap.tpp"
 
 #endif
+
+
+/*template <typename Comparable>
+void maxHeap (vector<Comparable> &array, int node, int n) {
+	int child;
+	Comparable tmp = std::move(array[node]);
+
+	 // Continues on the left side of the tree until a leaf is found
+	for (; node * 2 + 1 < currentSize; node = child) {
+        child = node * 2 + 1;
+		 // Find the larger child if the right child exists
+        if (child != currentSize - 1 && array[child + 1] > array[child]) // Change the comparison to find the larger child
+            ++child;
+		 // If the larger child is greater than the current node, swap them
+        if (array[child] > tmp)
+            array[node] = std::move(array[child]);
+        else
+		 // if everything is as it should the heap will break
+            break;
+	}
+	// Place the original value of the current node in its correct position
+	array[node] = std::move(tmp);*/
